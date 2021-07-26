29b84fb62ede7ed73751518dab7c5bd67c73daf8
AndreevG <andreev.gs12@yandex.by>
2021-07-26 18:19:09 +0300

    change navigation

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+import store from './store';
+import AuthScreen from './screens/AuthScreen';
+import WelcomeScreen from './screens/WelcomeScreen';
+import MapScreen from './screens/MapScreen';
+import DeckScreen from './screens/DeckScreen';
+import SettingsScreen from './screens/SettingsScreen';
+import ReviewScreen from './screens/ReviewScreen';
+import {Provider} from "react-redux";
+
+const Tab = createBottomTabNavigator();
+const ReviewStack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Provider store={store}>
+          <NavigationContainer>
+              <Tab.Navigator>
+                  <Tab.Screen name="Welcome" component={WelcomeScreen}/>
+                  <Tab.Screen name="Auth" component={AuthScreen}/>
+                  {/*<Tab.Screen name="Main" component={*/}
+                  {/*    <NavigationContainer>*/}
+                  {/*        <Tab.Navigator>*/}
+                  {/*            <Tab.Screen name="Map" component={MapScreen}/>*/}
+                  {/*            <Tab.Screen name="Deck" component={DeckScreen}/>*/}
+                  {/*            <Tab.Screen name="Review" component={*/}
+                  {/*                <ReviewStack.Navigator>*/}
+                  {/*                    <ReviewStack.Screen name="Settings" component={ReviewScreen} />*/}
+                  {/*                    <ReviewStack.Screen name="Details" component={SettingsScreen} />*/}
+                  {/*                </ReviewStack.Navigator>*/}
+                  {/*            }/>*/}
+                  {/*        </Tab.Navigator>*/}
+                  {/*    </NavigationContainer>*/}
+                  {/*}/>*/}
+              </Tab.Navigator>
+          </NavigationContainer>
+      </Provider>
+
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
